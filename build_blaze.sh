926caa7016753f4e529fd3da3bc16f8d1fac0391
GuneetAtwal <guneetatwal@gmail.com>
2016-08-25 17:57:03 +0530

    update: Build Script * Add recompile option * Add make clean option * Add housekeeping * Strip modules for size * improvement in logic * Cleaner code with new colors * Different color for different phase * Fixes
    Signed-off-by: GuneetAtwal <guneetatwal@gmail.com>

--- build_blaze.sh
+++ build_blaze.sh
@@ -3,8 +3,12 @@
 # Custom Kernel build script
 
 # Constants
-CPU_THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
 green='\033[01;32m'
+red='\033[01;31m'
+blink_red='\033[05;31m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
 default='\033[0m'
 
 # Resources
@@ -14,6 +18,7 @@ IMAGE=$KERNEL_DIR/arch/arm64/boot/Image
 #IMAGE=$KERNEL_DIR/arch/arm/boot/zImage for 32 bit architecture
 DTBTOOL=$KERNEL_DIR/scripts/dtbTool
 TOOLCHAIN=$ANDROID_DIR/toolchain/aarch64-linux-android-4.9/bin
+STRIP=$TOOLCHAIN/aarch64-linux-android-strip
 
 #Paths
 OUT_DIR=$KERNEL_DIR/out
@@ -33,43 +38,108 @@ DEFCONFIG="blaze_defconfig"
 export LOCALVERSION=~`echo $BLAZE_VER`
 export CROSS_COMPILE=$TOOLCHAIN/aarch64-linux-android-
 export ARCH=arm64
-export SUBARCH=arm64
 export KBUILD_BUILD_USER="GuneetAtwal"
 export KBUILD_BUILD_HOST="BeastPC"
 
 function make_blaze {
-		echo -e "$green***********************************************"
+		echo -e "$green*******************************************************"
 		echo "                  Compiling $BLAZE_VER	              "
-		echo -e "***********************************************$default"
+		echo -e "*****************************************************"
 		echo
 		make $DEFCONFIG
-		make $THREAD
+		make menuconfig
+		make -j5
 		rm -rf $NEWOUT/Image
 		cp -vr $IMAGE $NEW_OUT
 		make_dtb
 		strip_modules
 		make_zip
+		housekeeping
+		echo -e "$green*******************************************************"
+		echo "              Compilation Completed!!              "
+		echo -e "*****************************************************$default"
+		}
+		
+function make_clean {
+		echo -e "$green***********************************************"
+		echo "          Cleaning up object files and other stuff	              "
+		echo -e "***********************************************$default"
+		make mrproper
+		make_blaze
+	}
+		
+function make_recompile {
+			echo -e "$cyan*******************************************************"
+			echo "             Recompiling $BLAZE_VER	              "
+			echo -e "*****************************************************"
+			make -j5
+			rm -rf $NEWOUT/Image
+			cp -vr $IMAGE $NEW_OUT
+			make_dtb
+			strip_modules
+			make_zip
+			housekeeping
+		}
+		
+function make_dtb {
+			echo -e "$blue*******************************************************"
+			echo "             		Creating dt.img....	              "
+			echo -e "*****************************************************"
+			rm -rf $NEWOUT/dt.img
+			$DTBTOOL -v -s 2048 -o $NEW_OUT/dt.img -p scripts/dtc/ arch/arm/boot/dts/
 		}
 
-function make_dtb {
-		rm -rf $NEWOUT/dt.img
-		$DTBTOOL -v -s 2048 -o $NEW_OUT -p scripts/dtc/ arch/arm/boot/dts/
+function strip_modules {
+		rm -rf $MODULE_DIR/*
+		mkdir $MODULE_DIR/blaze
+		find . -path ./build -prune -o -name '*.ko' -print | xargs cp -t $MODULE_DIR/
+		cd $MODULE_DIR
+		echo -e "$cyan*******************************************************"
+		echo "             Stripping modules for size....	              "
+		echo -e "*****************************************************"
+		$STRIP --strip-unneeded *.ko
+		mv $MODULE_DIR/wlan.ko $MODULE_DIR/blaze/blaze_wlan.ko
+		ln -s /system/lib/modules/blaze/blaze_wlan.ko $MODULE_DIR/wlan.ko
+		cd $KERNEL_DIR
 		}
 
 function make_zip {
+		echo -e "$yellow*******************************************************"
+		echo "             		Zipping up....	              "
+		echo -e "*****************************************************"
 		cd $OUT_DIR
-		zip -r9 `echo $BLAZE_VER`.zip *
-		mv  `echo $BLAZE_VER`.zip $OUT_ZIP
+		rm -f '*.zip'
+		zip -yr BlazeKernel`echo $CUR_VER`.zip *
+		mv BlazeKernel`echo $CUR_VER`.zip $OUT_ZIP
+		echo "       Find your zip in Blaze-Releases directory"
+		echo -e "$default"
 		cd $KERNEL_DIR 
 		}
 
+function housekeeping {
+		echo -e "$green*******************************************************"
+		echo "            Cleaning up the mess created...	              "
+		echo -e "*****************************************************$default"
+		rm -rf $NEW_OUT/Image
+		rm -rf $NEW_OUT/dt.img
+	}
+		
+
 BUILD_START=$(date +"%s")
-while read -p "Do you want to build kernel (y/n)? " choice
+while read -p " 'Y' to Compile all , 'R' to clean and recompile , 'C' to to do a clean compilation 'N' to exit " choice
 do
 case "$choice" in
 	y|Y)
 		make_blaze
 		break
+		;;
+	r|R )
+		make_recompile
+		break
+		;;
+	c|C )
+		make_clean
+		break;
 		;;
 	n|N )
 		break
